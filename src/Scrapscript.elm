98802461db701b7323dea126351002eafa46f7f7
Taylor Troesh <surprisetalk@gmail.com>
2024-09-26 08:29:45 -0700

    ☀

--- src/Scrapscript.elm
+++ src/Scrapscript.elm
@@ -7,6 +7,8 @@ import Dict exposing (Dict)
 import Parser exposing ((|.), (|=), Parser, Step(..), end, lazy, oneOf, succeed, symbol)
 import Parser.Extras as Parser
 import Set exposing (Set)
+import Dict exposing (Dict)
+import Result exposing (Result)
 
 
 type Token
@@ -197,7 +199,6 @@ type Scrap
     | Closure ( Env, Scrap )
     | EnvObject Env
 
-
 type BinopKind
     = ADD
     | SUB
@@ -235,10 +236,336 @@ false =
     Variant ( "false", List [] )
 
 
+type alias Prec =
+    { pl : Float
+    , pr : Float
+    }
+
+type alias ParserState =
+    { tokens : List Token
+    , counter : Int
+    }
+
+ps : Dict String Prec
+ps =
+    Dict.fromList
+        [ ( "::", { pl = 2000, pr = 2000 } )
+        , ( "@", { pl = 1001, pr = 1001.1 } )
+        , ( "", { pl = 1000, pr = 1000.1 } )
+        -- ... (other operators omitted for brevity)
+        ]
+
+highestPrec : Float
+highestPrec =
+    ps
+        |> Dict.values
+        |> List.map (\p -> max p.pl p.pr)
+        |> List.maximum
+        |> Maybe.withDefault 0
+
+gensym : ParserState -> ( String, ParserState )
+gensym state =
+    let
+        newCounter =
+            state.counter + 1
+    in
+    ( "$v" ++ String.fromInt newCounter
+    , { state | counter = newCounter }
+    )
+
 parse : List Token -> Result String Scrap
 parse tokens =
-    Err "TODO: parse"
+    case parseHelper { tokens = tokens, counter = -1 } 0 of
+        Ok ( obj, _ ) ->
+            Ok obj
 
+        Err err ->
+            Err err
+
+parseHelper : ParserState -> Float -> Result String ( Scrap, ParserState )
+parseHelper state p =
+    Result.andThen (parseInfixAndApplication state p) <|
+    case state.tokens of
+        [] ->
+            Err "Unexpected EOF"
+
+        token :: restTokens ->
+            let
+                newState =
+                    { state | tokens = restTokens }
+            in
+            case token of
+                IntLit value ->
+                    Ok ( Int value, newState )
+
+                FloatLit value ->
+                    Ok ( Float value, newState )
+
+                Name value ->
+                    Ok ( Var value, newState )
+
+                VariantToken value ->
+                    parseHelper newState (Dict.get "" ps |> Maybe.map .pr |> Maybe.withDefault 0 |> (+) 1)
+                        |> Result.map (\( obj, s ) -> ( Variant (value, obj), s ))
+
+                BytesLit ( value, base ) ->
+                    case base of
+                        85 ->
+                            Ok ( Bytes (value), newState )
+
+                        64 ->
+                            Ok ( Bytes value, newState )
+
+                        32 ->
+                            Ok ( Bytes (value), newState )
+
+                        16 ->
+                            Ok ( Bytes (value), newState )
+
+                        _ ->
+                            Err ("Unexpected base " ++ String.fromInt base)
+
+                StringLit value ->
+                    Ok ( String value, newState )
+
+                Operator "..." ->
+                    case newState.tokens of
+                        (Name name) :: rest ->
+                            Ok ( Spread (Just name), { newState | tokens = rest } )
+
+                        _ ->
+                            Ok ( Spread Nothing, newState )
+
+                Operator "|" ->
+                    parseMatchFunction newState
+
+                LeftParen ->
+                    case newState.tokens of
+                        (RightParen :: rest) ->
+                            Ok ( Hole, { newState | tokens = rest } )
+
+                        _ ->
+                            parseHelper newState 0
+                                |> Result.andThen (\( obj, s ) ->
+                                    case s.tokens of
+                                        (RightParen :: rest) ->
+                                            Ok ( obj, { s | tokens = rest } )
+
+                                        _ ->
+                                            Err "TODO: unexpected token" -- (UnexpectedToken (Maybe.withDefault LeftParen (List.head s.tokens)))
+                                )
+
+                LeftBracket ->
+                    parseList newState
+
+                LeftBrace ->
+                    parseRecord newState
+
+                Operator "-" ->
+                    parseHelper newState (highestPrec + 1)
+                        |> Result.map (\( obj, s ) -> ( Binop SUB (Int 0, obj), s ))
+
+                _ ->
+                    Err "TODO: unexpected token" -- (UnexpectedToken token)
+
+parseRecord : ParserState -> Result String ( Scrap, ParserState )
+parseRecord state =
+    let
+        parseRecordHelper : ParserState -> List (String, Scrap) -> Result String ( Scrap, ParserState )
+        parseRecordHelper s acc =
+            case s.tokens of
+                (RightBrace :: rest) ->
+                    Ok ( Record acc, { s | tokens = rest } )
+
+                _ ->
+                    parseAssign s
+                        |> Result.andThen (\s_ -> case s_ of
+                            ( Assign ((Var name), value), newState ) ->
+                              case newState.tokens of
+                                  (RightBrace :: rest) ->
+                                      Ok ( Record ((name, value) :: acc), { newState | tokens = rest } )
+
+                                  (Operator "," :: rest) ->
+                                      parseRecordHelper { newState | tokens = rest } ((name, value) :: acc)
+
+                                  _ ->
+                                      Err "TODO: unexpected token" -- (UnexpectedToken (Maybe.withDefault LeftBrace (List.head newState.tokens)))
+                            _ ->
+                                      Err ("TODO: unexpected record deconstruction")
+                        )
+    in
+    parseRecordHelper state []
+
+parseList : ParserState -> Result String ( Scrap, ParserState )
+parseList state =
+    let
+        parseListHelper : ParserState -> List Scrap -> Result String ( Scrap, ParserState )
+        parseListHelper s acc =
+            case s.tokens of
+                (RightBracket :: rest) ->
+                    Ok ( List (List.reverse acc), { s | tokens = rest } )
+
+                _ ->
+                    parseHelper s 2
+                        |> Result.andThen (\( item, newState ) ->
+                            case newState.tokens of
+                                (RightBracket :: rest) ->
+                                    Ok ( List (List.reverse (item :: acc)), { newState | tokens = rest } )
+
+                                (Operator "," :: rest) ->
+                                    parseListHelper { newState | tokens = rest } (item :: acc)
+
+                                _ ->
+                                    Err "TODO: unexpected token" -- (UnexpectedToken (Maybe.withDefault LeftBracket (List.head newState.tokens)))
+                        )
+    in
+    parseListHelper state []
+
+parseAssign : ParserState -> Result String ( Scrap, ParserState )
+parseAssign state =
+    parseHelper state 0
+        |> Result.andThen (\( obj, newState ) ->
+            case obj of
+                Spread _ ->
+                    Ok ( Assign (Var "...", obj), newState )
+
+                _ ->
+                    case newState.tokens of
+                        (Operator "=" :: rest) ->
+                            parseHelper { newState | tokens = rest } (getPrec "=").pl
+                                |> Result.map (\( value, s ) -> ( Assign (obj, value), s ))
+
+                        _ ->
+                            Err ("Failed to parse variable assignment in record constructor")
+        )
+
+parseMatchFunction : ParserState -> Result String ( Scrap, ParserState )
+parseMatchFunction state =
+    let
+        parseCases : ParserState -> List MatchCase -> Result String ( Scrap, ParserState )
+        parseCases s acc =
+            parseHelper s (getPrec "|").pl
+                |> Result.andThen (\( expr, newState ) ->
+                    case expr of
+                        Function (arg, body) ->
+                            let
+                                newCase =
+                                    MatchCase (arg, body)
+                            in
+                            case newState.tokens of
+                                (Operator "|" :: rest) ->
+                                    parseCases { newState | tokens = rest } (newCase :: acc)
+
+                                _ ->
+                                    Ok ( MatchFunction (List.reverse (newCase :: acc)), newState )
+
+                        _ ->
+                            Err ("Expected function in match expression")
+                )
+    in
+    parseCases state []
+
+getPrec : String -> Prec
+getPrec op =
+    Dict.get op ps |> Maybe.withDefault { pl = 0, pr = 0 }
+
+parseInfixAndApplication : ParserState -> Float -> ( Scrap, ParserState ) -> Result String ( Scrap, ParserState )
+parseInfixAndApplication originalState minPrec ( left, state ) =
+    case state.tokens of
+        [] ->
+            Ok ( left, state )
+
+        (Operator op :: rest) ->
+            let
+                prec =
+                    getPrec op
+            in
+            if prec.pl < minPrec then
+                Ok ( left, state )
+            else
+                parseHelper { state | tokens = rest } prec.pr
+                    |> Result.andThen (\( right, newState ) ->
+                        case op of
+                            "=" ->
+                                case left of
+                                    Var _ ->
+                                        parseInfixAndApplication originalState minPrec ( Assign (left, right), newState )
+
+                                    _ ->
+                                        Err ("Expected variable in assignment")
+
+                            "->" ->
+                                parseInfixAndApplication originalState minPrec ( Function (left, right), newState )
+
+                            "|>" ->
+                                parseInfixAndApplication originalState minPrec ( Apply (right, left), newState )
+
+                            "<|" ->
+                                parseInfixAndApplication originalState minPrec ( Apply (left, right), newState )
+
+                            ">>" ->
+                                let
+                                    ( varName, genState ) =
+                                        gensym newState
+
+                                    newFunc =
+                                        Function ((Var varName), (Apply (right, (Apply (left, (Var varName))))))
+                                in
+                                parseInfixAndApplication originalState minPrec ( newFunc, genState )
+
+                            "<<" ->
+                                let
+                                    ( varName, genState ) =
+                                        gensym newState
+
+                                    newFunc =
+                                        Function ((Var varName), (Apply (left, (Apply (right, (Var varName))))))
+                                in
+                                parseInfixAndApplication originalState minPrec ( newFunc, genState )
+
+                            "." ->
+                                parseInfixAndApplication originalState minPrec ( Where (left, right), newState )
+
+                            "?" ->
+                                parseInfixAndApplication originalState minPrec ( Assert (left, right), newState )
+
+                            "@" ->
+                                parseInfixAndApplication originalState minPrec ( Access (left, right), newState )
+
+                            _ ->
+                                parseInfixAndApplication originalState minPrec ( Binop (toBinopKind op) (left, right), newState )
+                    )
+
+        _ ->
+            let
+                prec =
+                    getPrec ""
+            in
+            if prec.pl < minPrec then
+                Ok ( left, state )
+            else
+                parseHelper state prec.pr
+                    |> Result.andThen (\( right, newState ) ->
+                        parseInfixAndApplication originalState minPrec ( Apply (left, right), newState )
+                    )
+
+toBinopKind : String -> BinopKind
+toBinopKind op =
+    case op of
+        "+" -> ADD
+        "-" -> SUB
+        "*" -> MUL
+        "/" -> DIV
+        "%" -> MOD
+        "&&" -> BOOL_AND
+        "||" -> BOOL_OR
+        "==" -> EQUAL
+        "/=" -> NOT_EQUAL
+        "<" -> LESS
+        ">" -> GREATER
+        "<=" -> LESS_EQUAL
+        ">=" -> GREATER_EQUAL
+        _ -> Debug.todo ("Unhandled operator: " ++ op)
 
 match : ( Scrap, Scrap ) -> Result String (List ( String, Scrap ))
 match ( exp, pattern ) =
